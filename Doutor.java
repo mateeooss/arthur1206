b16eee20a4549cddc1729c06f69178857910feae
mateeooss <matheus_limaalmeida3@hotmail.com>
2020-06-12 23:57:49 -0300

    Create Doutor.java

---
+++ Doutor.java
@@ -0,0 +1,15 @@
+package unicesumar.segundoBimestre;
+
+import java.util.ArrayList;
+
+public class Doutor {
+	private String nome;
+	private String cpf;
+
+
+	public Doutor(String nome, String cpf) {
+		this.nome = nome;
+		this.cpf = cpf;
+	}
+
+}
